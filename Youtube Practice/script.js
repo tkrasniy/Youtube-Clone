806abaf3030582a12ada14bf5080a00878729505
tkrasniy <119018846+tkrasniy@users.noreply.github.com>
2023-01-27 18:18:44 -0800

    Add files via upload

---
+++ Youtube Practice/script.js
@@ -0,0 +1,6 @@
+var menuIcon = document.querySelector(".menu-icon");
+var sideBar = document.querySelector(".sidebar");
+
+menuIcon.onclick =function(){
+    sideBar.classList.toggle("small-sidebar")
+}
